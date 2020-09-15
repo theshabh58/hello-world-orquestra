e6af5742e18d66735a6c59cc33847a3a56b16c25
theshabh58 <rishabhpatel58@gmail.com>
2020-09-15 10:06:53 -0500

    updated workflow

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    #Package which can be refrenced in the template
-    name = "orquestra",
-    #Tells installer to look for sub-dr called python which contains the source code
-    packages = setuptools.find_packages(where="python"),
-    #allow the python dir to be imported without the python path prefix
-    package_dir = {
+    name                            = "orquestra",
+    packages                        = setuptools.find_packages(where = "python"),
+    package_dir                     = {
         "" : "python"
     },
-    classifiers = (
+    classifiers                     = (
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
