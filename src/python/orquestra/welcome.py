37b924702fa15109481d318462ca7de64dbd1d35
Rishabh Patel <rishabhpatel58@gmail.com>
2021-05-27 14:30:57 -0500

    Update welcome.py

--- src/python/orquestra/welcome.py
+++ src/python/orquestra/welcome.py
@@ -21,12 +21,12 @@ def welcome(ref_artifact_1, ref_artifact_2):
         print(os.environ)
         print("USER:" + pwd.getpwuid( os.getuid() )[ 0 ])
         subprocess.call(['pwd'])
-        #time.sleep(3600)
+        time.sleep(3600)
     for i in range(10):
         resp = requests.get("https://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json")
         print(f"HTTP: {resp}")
         for j in range(5):
-            time.sleep(0.1)
+            time.sleep(300)
             response = requests.get("https://api.spacexdata.com/v4/launches/latest")
             print(f"HTTPS: {response}")
             
